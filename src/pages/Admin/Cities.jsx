6112f27fdc456c82eef13aa0659b890a5c947dfe
Laur <qlaurd@gmail.com>
2023-07-25 18:08:18 +0300

    fix 2

--- src/pages/Admin/Cities.jsx
+++ src/pages/Admin/Cities.jsx
@@ -5,7 +5,7 @@ import { useFormik } from "formik"
 import * as Yup from "yup"
 import { useNavigate } from "react-router-dom"
 import DropDownForm from "../../components/DropDownForm"
-import Filtru from '../../components/FIltru'
+import Filtru from '../../components/Filtru'
 import SearchBar from '../../components/SearchBar'
 import AdminItem from '../../components/AdminItem'
 import useAxiosPrivate from '../../hooks/useAxiosPrivate'
